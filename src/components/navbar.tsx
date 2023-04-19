e7d30f0b4473a1cfff4913b91c58d516044c13a8
Francisco Berti da Cruz <fbc991@live.com>
2023-04-18 22:29:17 -0300

    ok

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,34 +1,48 @@
-import { Link } from 'react-router-dom'
-import { auth } from '../config/firebase'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { signOut } from 'firebase/auth'
+import { Link } from 'react-router-dom';
+import { auth } from '../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
 
 export const Navbar = () => {
+  const [user] = useAuthState(auth);
 
-    const [user] = useAuthState(auth)
+  const signUserOut = async () => {
+    await signOut(auth);
+  };
 
-    const signUserOut = async () => {
-        await signOut(auth)
-    }
+  return (
+    <div className="navbar">
+      <div className="links">
+        <Link className="lLinks" to="/">
+          Home{' '}
+        </Link>
+        {!user ? (
+          <Link className="lLinks" to="/login">
+            Login{' '}
+          </Link>
+        ) : (
+          <Link className="lLinks" to="/createpost">
+            Create post{' '}
+          </Link>
+        )}
+      </div>
 
-    return (
-        <div className='navbar'>
-            <div className='links'>
-                <Link className='lLinks' to='/'>Home </Link>
-                {!user ? <Link className='lLinks' to='/login'>Login </Link>
-                    : <Link className='lLinks' to='/createpost'>Create post </Link>
-                }
-            </div>
-
-            <div className='user'>
-                {user && (
-                    <>
-                        <p className='userItem'>{user?.displayName}</p>
-                        <img className='userItem' src={user?.photoURL || ''} width='20' height='20' />
-                        <button className='userItem' onClick={signUserOut}>Log out</button>
-                    </>
-                )}
-            </div>
-        </div>
-    )
-}
+      <div className="user">
+        {user && (
+          <>
+            <p className="userItem">{user?.displayName}</p>
+            <img
+              className="userItem"
+              src={user?.photoURL || ''}
+              width="20"
+              height="20"
+            />
+            <button className="userItem" onClick={signUserOut}>
+              Log out
+            </button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
